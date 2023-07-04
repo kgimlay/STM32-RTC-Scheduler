57cf54d97b0c5f9cc31777c4ffc0b9a9c65dc1de
Kevin Imlay <kgimlay@gmail.com>
2023-07-04 15:59:59 -0700

    Fixed inconsistent tabs/spaces across files

--- Code/Desktop Application/SerialConnection.py
+++ Code/Desktop Application/SerialConnection.py
@@ -18,76 +18,76 @@ DEFAULT_EXCLUSIVE = False
 
 
 class SerialConnection:
-	# UART Connection encapsulates the most basic functions for sending and
-	# receiving data over the UART to the STM32 MCU.  Communication is
-	# performed using the pySerial package.
+    # UART Connection encapsulates the most basic functions for sending and
+    # receiving data over the UART to the STM32 MCU.  Communication is
+    # performed using the pySerial package.
 
-	# serial connection parameters
-	_connection = None
-
-
-	def __init__(self):
-		# Initialize new connection object with defualt serial parameters.
-		# These parameters are what are also programmed into the MCU.
-
-		# Create new serial object
-		self._connection = serial.Serial()
-
-		# Set parameters for serial communication.
-		self._connection.baudrate = DEFAULT_BAUD
-		self._connection.bytesize = DEFAULT_BYTESIZE
-		self._connection.parity = DEFAULT_PARITY
-		self._connection.stopbits = DEFAULT_STOPBITS
-		self._connection.timeout = DEFAULT_READ_TIMEOUT
-		self._connection.write_timeout = DEFAULT_WRITE_TIMEOUT
-		self._connection.xonxoff = DEFAULT_SOFT_FLOW_CONTRL
-		self._connection.rtscts = DEFAULT_RTSCTS_FLOW_CONTRL
-		self._connection.dsrdtr = DEFAULT_DSRDTR_FLOW_CONTRL
-		self._connection.inter_byte_timeout = DEFAULT_INTER_BYTE_TIMEOUT
-		self._connection.exclusive = DEFAULT_EXCLUSIVE
+    # serial connection parameters
+    _connection = None
 
 
-	def openPort(self, port):
-		# Alias to open a serial connection.  The port must be a string
-		# representation of the directory and port to open on the OS.
+    def __init__(self):
+        # Initialize new connection object with defualt serial parameters.
+        # These parameters are what are also programmed into the MCU.
 
-		# Test for valid port parameter.
-		if not isinstance(port, str): raise TypeError
+        # Create new serial object
+        self._connection = serial.Serial()
 
-		# Set port field for future purposes.
-		self._connection.port = port
-
-		# Try to open connection.
-		self._connection.open()
-
-
-	def closePort(self):
-		# Alias to close serial connection.
-
-		# Try to close connection.
-		self._connection.close()
-
-
-	def send(self, message):
-		# Alias to send a message over the serial connection.  The message
-		# must be a string that can be encoded to ASCII.
-
-		# Test for valid message parameter.
-		if not isinstance(message, str): raise TypeError
-
-		# Encode message and send message.  Ensure message is sent before 
-		# continuing.
-		self._connection.write(message.encode('ascii'))
-		self._connection.flush()
+        # Set parameters for serial communication.
+        self._connection.baudrate = DEFAULT_BAUD
+        self._connection.bytesize = DEFAULT_BYTESIZE
+        self._connection.parity = DEFAULT_PARITY
+        self._connection.stopbits = DEFAULT_STOPBITS
+        self._connection.timeout = DEFAULT_READ_TIMEOUT
+        self._connection.write_timeout = DEFAULT_WRITE_TIMEOUT
+        self._connection.xonxoff = DEFAULT_SOFT_FLOW_CONTRL
+        self._connection.rtscts = DEFAULT_RTSCTS_FLOW_CONTRL
+        self._connection.dsrdtr = DEFAULT_DSRDTR_FLOW_CONTRL
+        self._connection.inter_byte_timeout = DEFAULT_INTER_BYTE_TIMEOUT
+        self._connection.exclusive = DEFAULT_EXCLUSIVE
 
 
-	def receive(self, length):
-		# Alias to receive a message from the serial connection.  The length
-		# must be an integer greater than 0.
+    def openPort(self, port):
+        # Alias to open a serial connection.  The port must be a string
+        # representation of the directory and port to open on the OS.
 
-		# Test for valid length parameter.
-		if not isinstance(length, int): TypeError
-		if length < 1: raise ValueError
+        # Test for valid port parameter.
+        if not isinstance(port, str): raise TypeError
 
-		# Read from the serial connection, decode, and return string.
-		return self._connection.read(length).decode('ascii')
+        # Set port field for future purposes.
+        self._connection.port = port
+
+        # Try to open connection.
+        self._connection.open()
+
+
+    def closePort(self):
+        # Alias to close serial connection.
+
+        # Try to close connection.
+        self._connection.close()
+
+
+    def send(self, message):
+        # Alias to send a message over the serial connection.  The message
+        # must be a string that can be encoded to ASCII.
+
+        # Test for valid message parameter.
+        if not isinstance(message, str): raise TypeError
+
+        # Encode message and send message.  Ensure message is sent before 
+        # continuing.
+        self._connection.write(message.encode('ascii'))
+        self._connection.flush()
+
+
+    def receive(self, length):
+        # Alias to receive a message from the serial connection.  The length
+        # must be an integer greater than 0.
+
+        # Test for valid length parameter.
+        if not isinstance(length, int): TypeError
+        if length < 1: raise ValueError
+
+        # Read from the serial connection, decode, and return string.
+        return self._connection.read(length).decode('ascii')
--- Code/Desktop Application/SerialProtocol.py
+++ Code/Desktop Application/SerialProtocol.py
@@ -11,125 +11,125 @@ MESSAGE_LENGTH = 32
 
 
 class PortException(Exception):
-	# 
-	def __init__(self, errMessage):
-		super().__init__(self, errMessage)
+    # 
+    def __init__(self, errMessage):
+        super().__init__(self, errMessage)
 
 
 def _openPort(port):
-	# 
+    # 
 
-	# create new UART Connection on port
-	tempConnection = SerialConnection.SerialConnection()
-	try:
-		# attempt to open port
-		tempConnection.openPort(port)
+    # create new UART Connection on port
+    tempConnection = SerialConnection.SerialConnection()
+    try:
+        # attempt to open port
+        tempConnection.openPort(port)
 
-	# raise an exception that the port could not be opened
-	except serial.SerialException:
-		raise errMessage('Port {} could not be opened.'.format(port))
+    # raise an exception that the port could not be opened
+    except serial.SerialException:
+        raise errMessage('Port {} could not be opened.'.format(port))
 
-	# return connection
-	return tempConnection
+    # return connection
+    return tempConnection
 
 
 def _closePort(connection):
-	#
+    #
 
-	# close the port
-	connection.closePort()
+    # close the port
+    connection.closePort()
 
 
 def _handshake(connection):
-	# 
-	try:
-		# compose acknowledge message
-		synMessage = SerialPacket.SerialPacket(MESSAGE_LENGTH, HEADER_LENGTH, 'SNYC', '')
-		sendData = synMessage.format()
-		
-		# send acknowledge message
-		connection.send(sendData)
-		
-		# listen for echo back
-		receivedData = connection.receive(MESSAGE_LENGTH)
-		synackMessage = SerialPacket.SerialPacket(MESSAGE_LENGTH, HEADER_LENGTH, receivedData)
+    # 
+    try:
+        # compose acknowledge message
+        synMessage = SerialPacket.SerialPacket(MESSAGE_LENGTH, HEADER_LENGTH, 'SNYC', '')
+        sendData = synMessage.format()
+        
+        # send acknowledge message
+        connection.send(sendData)
+        
+        # listen for echo back
+        receivedData = connection.receive(MESSAGE_LENGTH)
+        synackMessage = SerialPacket.SerialPacket(MESSAGE_LENGTH, HEADER_LENGTH, receivedData)
 
-		# test that received message is echoed correctly
-		if synackMessage == synMessage:
-			# return successful handshake
-			return True
+        # test that received message is echoed correctly
+        if synackMessage == synMessage:
+            # return successful handshake
+            return True
 
-		else:
-			# return handshake unsuccessful
-			return False
+        else:
+            # return handshake unsuccessful
+            return False
 
-	# Raise PortException if a serial exception occured
-	except serial.SerialException:
-		raise PortException('Handshake failed due to port becomming unavailable.')
+    # Raise PortException if a serial exception occured
+    except serial.SerialException:
+        raise PortException('Handshake failed due to port becomming unavailable.')
 
-	# Raise Port Exception if malformed response from MCU
-	except AssertionError:
-		raise PortException('Malformed message from MCU.')
+    # Raise Port Exception if malformed response from MCU
+    except AssertionError:
+        raise PortException('Malformed message from MCU.')
 
-	# Raise a PortException is a timeout exception occured
-	except serial.SerialTimeoutException:
-		raise PortException('Handshake failed due to timeout.')
+    # Raise a PortException is a timeout exception occured
+    except serial.SerialTimeoutException:
+        raise PortException('Handshake failed due to timeout.')
 
 
 class SerialProtocol:
-	# 
+    # 
 
-	# connection object
-	_connection = None
-
-
-	def __new__(cls, port):
-		# Attempts to open a connection on the port provided.  If successful,
-		# a SerialProtocol object is created.  If not, an exception is thrown.
-
-		# check parameter
-		assert isinstance(port, str), 'Port parameter must be a string.'
-
-		# Create new UART Connection on port.
-		# Throws an exception if the port could not be opened, which is
-		# left unhandled here to be passed up to caller.
-		tempConnection = _openPort(port)
-
-		# attempt handshake with port
-		# if handshake successful, then create object
-		if _handshake(tempConnection):
-			instance = super().__new__(cls)
-			instance.__init__(port)
-			instance._connection = tempConnection
-			return instance
-
-		# if handshake unsuccessful, return None
-		else:
-			return None
+    # connection object
+    _connection = None
 
 
-	def __init__(self, port):
-		# 
+    def __new__(cls, port):
+        # Attempts to open a connection on the port provided.  If successful,
+        # a SerialProtocol object is created.  If not, an exception is thrown.
 
-		#
-		pass
+        # check parameter
+        assert isinstance(port, str), 'Port parameter must be a string.'
+
+        # Create new UART Connection on port.
+        # Throws an exception if the port could not be opened, which is
+        # left unhandled here to be passed up to caller.
+        tempConnection = _openPort(port)
+
+        # attempt handshake with port
+        # if handshake successful, then create object
+        if _handshake(tempConnection):
+            instance = super().__new__(cls)
+            instance.__init__(port)
+            instance._connection = tempConnection
+            return instance
+
+        # if handshake unsuccessful, return None
+        else:
+            return None
 
 
-	def __del__(self):
-		# 
+    def __init__(self, port):
+        # 
 
-		# close connection
-		_closePort(self._connection)
+        #
+        pass
 
 
-	def echo(self, messageText):
-		# 
+    def __del__(self):
+        # 
 
-		message = SerialPacket.SerialPacket(
-			MESSAGE_LENGTH, HEADER_LENGTH, 'ECHO', '{}'.format(messageText))
-		self._connection.send(message.format())
-		
-	def receive(self):
-		# 
+        # close connection
+        _closePort(self._connection)
 
-		return self._connection.receive(MESSAGE_LENGTH)[HEADER_LENGTH:]
+
+    def echo(self, messageText):
+        # 
+
+        message = SerialPacket.SerialPacket(
+            MESSAGE_LENGTH, HEADER_LENGTH, 'ECHO', '{}'.format(messageText))
+        self._connection.send(message.format())
+        
+    def receive(self):
+        # 
+
+        return self._connection.receive(MESSAGE_LENGTH)[HEADER_LENGTH:]
